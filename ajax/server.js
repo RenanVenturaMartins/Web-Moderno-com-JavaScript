3154fb1c2fdf74fd299f98ec6994460ca0ffa61e
RenanVenturaMartins <40028922Ren@n>
2022-09-01 17:07:28 -0300

    licação com o front end

--- ajax/server.js
+++ ajax/server.js
@@ -7,6 +7,31 @@ app.use(express.static('.'))
 app.use(BodyParser.urlencoded({ extended: true}))
 app.use(BodyParser.json())
 
-app.get('/teste', (req, res) => res.send('ok'))
+
+const multer = require('multer')
+
+const storage = multer.diskStorage ({
+    destination: function (req, file, callback) {
+    callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+     callback(null, `${Date.now()}_${file.originalname}`)
+    }
+
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro')
+        }
+            res.end('Concluido com sucesso')
+        })
+    })
+
+
+
 
 app.listen(8080, () => console.log('executando...'))
